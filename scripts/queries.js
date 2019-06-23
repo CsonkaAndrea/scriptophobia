3a9f57c9a8b414ccf12c385a2a9a51340dc4da8d
Tamás Takács <tamas.takacs91@gmail.com>
2019-06-23 21:58:22 +0200

    már majdnem kész

--- scripts/queries.js
+++ scripts/queries.js
@@ -7,6 +7,7 @@ function milyenKlubok() {
         }
 
     }
+    klubok = klubok.sort();
     return klubok;
 }
 
@@ -14,7 +15,7 @@ function drawUnorderedList(melyikHalmaz) {
     var s = "";
 
     for (var i = 0; i < melyikHalmaz.length; i++) {
-        s += '<li>' + melyikHalmaz[i] + '</li>';
+        s += '<li class="csapatok">' + melyikHalmaz[i] + '</li>';
     }
 
     return s;
@@ -49,7 +50,7 @@ function hunPlayers() {
     var hunPlayersCount = 0;
 
     for (var i = 0; i < data.length; i++) {
-        if (data[i].magyar === true) {
+        if (data[i].magyar === true && data[i].kulfoldi === false) {
             hunPlayersCount += 1;
         }
     }
@@ -58,12 +59,29 @@ function hunPlayers() {
 document.querySelector("#hunPlayers").innerHTML = hunPlayers();
 
 function foreignPlayers() {
-    var allPlayers = data.length;
-    var foreignPlayersCount = data.length - hunPlayers();
+    var foreignPlayersCount = 0;
 
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].magyar === false && data[i].kulfoldi === true) {
+            foreignPlayersCount += 1;
+        }
+    }
     return foreignPlayersCount;
 }
 document.querySelector('#foreignPlayers').innerHTML = foreignPlayers();
+
+function doublePlayers() {
+    var doublePlayersCount = 0;
+
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].magyar === true && data[i].kulfoldi === true) {
+            doublePlayersCount += 1;
+        }
+    }
+    return doublePlayersCount;
+}
+document.querySelector('#doublePlayers').innerHTML = doublePlayers();
+
 
 /*function avgAgeOfPlayers () {
     var sumOfAge = 0;
@@ -82,6 +100,82 @@ function calcAge(szulido) {
     return age;
 }
 
+function AvgAge() {
+    var osszeg = 0;
+    var darab = data.length;
+    var atlag = 0;
+    for (var i = 0; i < data.length; i++) {
+        osszeg += calcAge(data[i].szulido);
+    }
+    atlag = osszeg / darab;
+    return atlag;
+}
+document.querySelector('#avgAge').innerHTML = parseInt(AvgAge()) + ' év';
+
+function legertekesebbJatekos() {
+    var legdragabb = data[0].ertek;
+    var dragaJatekos = [];
+
+    for (var i = 0; i < data.length; i++) {
+        if (legdragabb < data[i].ertek) {
+            legdragabb = data[i].ertek;
+        }
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (legdragabb === data[i].ertek) {
+            dragaJatekos.push({
+                lastName: data[i].vezeteknev,
+                firstName: data[i].utonev,
+                value: data[i].ertek
+            });
+        }
+    }
+    return dragaJatekos;
+}
+
+function mutasdALegdragabbat() {
+    var dragaPlayers = legertekesebbJatekos();
+    var s = '';
+
+    for (var i = 0; i < dragaPlayers.length; i++) {
+        s += dragaPlayers[i].lastName + ' ' + dragaPlayers[i].firstName + ', az értéke: ' + dragaPlayers[i].value + ' millió HUF.';
+    }
+    return s;
+}
+document.querySelector('#mostExpensive').innerHTML = mutasdALegdragabbat();
+
+function cheapestPlayers() {
+    var cheapest = data[0].ertek;
+    var olcsoPlayers = [];
+
+    for (var i = 0; i < data.length; i++) {
+        if (cheapest > data[i].ertek) {
+            cheapest = data[i].ertek;
+        }
+    }
+    for (var i = 0; i < data.length; i++) {
+        if (cheapest === data[i].ertek) {
+            olcsoPlayers.push({
+                lastName: data[i].vezeteknev,
+                firstName: data[i].utonev,
+                value: data[i].ertek
+            });
+        }
+    }
+    return olcsoPlayers;
+}
+
+function mutasdALegolcsobbat() {
+    var olcsoPlayers = cheapestPlayers();
+    var s = '';
+
+    for (var i = 0; i < olcsoPlayers.length; i++) {
+        s += olcsoPlayers[i].lastName + ' ' + olcsoPlayers[i].firstName + ', az értéke: ' + olcsoPlayers[i].value + ' millió HUF.';
+    }
+    return s;
+}
+document.querySelector('#cheapest').innerHTML = mutasdALegolcsobbat();
+
 function minimumAge() {
     var min = calcAge(data[0].szulido);
     for (var i = 0; i < data.length; i++) {
@@ -91,7 +185,6 @@ function minimumAge() {
     }
     return min;
 }
-document.querySelector('#youngest').innerHTML = minimumAge();
 
 function maximumAge() {
     var max = calcAge(data[0].szulido);
@@ -102,9 +195,8 @@ function maximumAge() {
     }
     return max;
 }
-document.querySelector('#oldest').innerHTML = maximumAge();
 
-function maximumAge() {
+/*function maximumAge() {
     var max = calcAge(data[0].szulido);
     for (var i = 0; i < data.length; i++) {
         if (calcAge(data[i].szulido) > max) {
@@ -113,6 +205,7 @@ function maximumAge() {
     }
     return max;
 }
+*/
 
 function oregEmber() {
     var max = maximumAge();
@@ -135,27 +228,13 @@ function mutasdAzIdoset() {
     s = '';
     document.querySelector('#legidosebbKor').innerHTML = 'A legidősebb életkor: ' + maximumAge();
 
-    for (i = 0; i < idosek.length; i++) {
+    for (var i = 0; i < idosek.length; i++) {
         s += '<li>' + idosek[i].lastName + ' ' + idosek[i].firstName + '</li>';
     }
     document.querySelector('#legidosebb').innerHTML = s;
     return s;
 }
 
-/* function fiatalEmber() {
-    var min = minimumAge();
-    var jatekosok = [];
-
-    for (var i = 0; i < data.length; i++) {
-        if (calcAge(data[i].szulido) === min) {
-            jatekosok.push(data[i].vezeteknev);
-            jatekosok.push(data[i].utonev);
-            jatekosok.push(minimumAge());
-        }
-    }
-    return jatekosok;
-}
-console.log(fiatalEmber()); */
 
 function fiatalEmber() {
     var min = minimumAge();
@@ -179,7 +258,7 @@ function mutasdAFiatalt() {
     s = '';
     document.querySelector('#legfiatalabbKor').innerHTML = 'A legfiatalabb életkor: ' + minimumAge();
 
-    for (i = 0; i < fiatalok.length; i++) {
+    for (var i = 0; i < fiatalok.length; i++) {
         s += '<li>' + fiatalok[i].lastName + ' ' + fiatalok[i].firstName + '</li>';
     }
     document.querySelector('#legfiatalabb').innerHTML = s;
@@ -188,7 +267,7 @@ function mutasdAFiatalt() {
 
 function positions() {
     var posztok = [];
-    for (i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (posztok.indexOf(data[i].poszt) == -1) {
             posztok.push(data[i].poszt)
         }
@@ -196,13 +275,13 @@ function positions() {
     return posztok;
 
 }
-console.log(positions());
+//console.log(positions());
 
 function posztokLista() {
     var posztok = positions();
     var s = '';
 
-    for (i = 0; i < posztok.length; i++) {
+    for (var i = 0; i < posztok.length; i++) {
         s += '<option value="' + posztok[i] + '">' + posztok[i] + '</option>'
     }
     return s;
@@ -212,7 +291,7 @@ document.querySelector('#positions').innerHTML = posztokLista();
 function posztonkentBontas() {
     var pozicio = document.querySelector('#positions').value
     var players = [];
-    for (i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (data[i].poszt === pozicio) {
             players.push(data[i].klub + ' - ' + data[i].vezeteknev + ' ' + data[i].utonev)
         }
@@ -220,14 +299,73 @@ function posztonkentBontas() {
     return players;
 }
 
+
 function listaPosztonkent() {
     var s = '';
     var players = posztonkentBontas().sort();
     document.querySelector('#valami').innerHTML = '';
 
-    for (i = 0; i < players.length; i++) {
+    for (var i = 0; i < players.length; i++) {
         s += '<li>' + players[i] + '</li>'
     }
     document.querySelector('#valami').innerHTML = s;
     return s;
 }
+
+function ertekCsokkeno() {
+    for (var i = 0; i < data.length - 1; i++) {
+        for (var j = i + 1; j < data.length; j++) {
+            if (data[i].ertek < data[j].ertek) {
+                var tmp = [data[i], data[j]];
+                data[i] = tmp[1];
+                data[j] = tmp[0];
+            }
+        }
+    }
+    return data;
+}
+
+function mutasdATopJatekosokat() {
+    var topPlayers = [];
+    var mindenki = ertekCsokkeno();
+
+    var hossz = ertekCsokkeno().length; // 312, az utolsó elem: 312-1
+    var kert = document.querySelector('#topAnyPlayer').value; //mondjuk 4
+    var kertSzam = hossz - kert; // 312 - 4 = 308
+
+    for (var i = 0; i < data.length - kertSzam; i++) {
+        topPlayers.push({
+            lastName: data[i].vezeteknev,
+            firstName: data[i].utonev,
+            value: data[i].ertek,
+            klub: data[i].klub
+        });
+    }
+    /*console.log(hossz);
+    console.log(kert);
+    console.log(kertSzam);
+    console.log(topPlayers);
+    */
+    return topPlayers;
+}
+
+function drawTopPlayers() {
+    var topPlayers = mutasdATopJatekosokat();
+    var s = '';
+
+    for (var i = 0; i < topPlayers.length; i++) {
+        s += '<li>' + topPlayers[i].lastName + ' ' + topPlayers[i].firstName + ', az értéke: ' + topPlayers[i].value + ' millió HUF.' + ' (' + topPlayers[i].klub + ')';
+        '</li>'
+    }
+    document.querySelector('#topPlayers').innerHTML = s;
+
+    return s;
+}
+
+/*fuction mutasdATopJatekosokat () {
+    var mindenki = ertekCsokkeno();
+    var topPlayers = [];
+
+    for ( var i = 0; i < data)
+}
+*/
